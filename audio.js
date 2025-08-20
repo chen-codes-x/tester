72d54e4fb3d13c2c63a8fe27df8a45f49cf1a74a
chen-codes-x <chanelclarke4122@gmail.com>
2025-08-20 10:10:17 +0100

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,9 @@
+class AudioControl {
+        constructor(){
+            this.charge = document.getElementById('charge'); 
+        }
+        play(sound){
+            sound.currentTime = 0; 
+            sound.play();
+        }
+    }
